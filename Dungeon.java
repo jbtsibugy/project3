e59153585dafe521c89851b0ab93ddf037f06072
Benedict Tsibu-Gyan <jtsibugy@mail.umw.edu>
2019-11-16 17:13:05 +0000

    Pushing every file again

--- Dungeon.java
+++ Dungeon.java
@@ -6,6 +6,9 @@ class Dungeon {
 		System.out.print("Enter your character's name: ");
 		String name = in.next();
 		Character user = new Character(name);
+              
+		System.out.println();
+	        System.out.println("This is the story of a guy who had a machine gun in a fantasy world for some reason");
 
 		Field.generateField(user);
 	}
