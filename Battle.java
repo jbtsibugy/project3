8f48bd854ca772a38464bf31a50ed2e2a31f28f1
Benedict Tsibu-Gyan <jtsibugy@mail.umw.edu>
2019-11-16 18:46:26 +0000

    Made each battle phase slower and easier to see

--- Battle.java
+++ Battle.java
@@ -4,11 +4,12 @@ class Battle {
 	//This method takes in a Character and an Enemy as input parameters
 	//and uses them to determine which of the two will still have
 	//remaining health after the battle
-	public static boolean winFight (Character c, Enemy e) {
+	public static boolean winFight (Character c, Enemy e) throws InterruptedException{
 		while (c.getHealth() > 0 && e.getHealth() > 0) {
 			System.out.println(c.getName() + " attacks for: " + c.getDamage());
 			e.setHealth(e.getHealth() - c.getDamage());
 			System.out.println("Enemy Health: " + e.getHealth());
+			Thread.sleep(2000);
 			System.out.println();
 
 			if (e.getHealth() <= 0) {
@@ -20,6 +21,7 @@ class Battle {
 			System.out.println("Enemy attacks for: " + e.getDamage());
 			c.setHealth(c.getHealth() - e.getDamage());
 			System.out.println(c.getName() + "'s health: " + c.getHealth());
+			Thread.sleep(2000);
 			System.out.println();
 
 			if (c.getHealth() <= 0) {
